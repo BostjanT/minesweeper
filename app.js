db2db9f04edab81644ce9ce04032172be8bc4bcb
BostjanT <36411197+BostjanT@users.noreply.github.com>
2021-04-19 10:26:57 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,37 +1,55 @@
 const grid = document.querySelector(".grid");
 const gameText = document.querySelector(".game-txt");
 const restartBtn = document.getElementById("game-restart");
+const flagsLeft = document.querySelector(".flags-left");
+const timer = document.querySelector(".timer");
 
 let width = 16;
 let bombs = 40;
+let flags = 0;
 let squares = [];
-let gameEnd = false;
+let isGameOver = false;
+let time = 0;
+flagsLeft.innerHTML = bombs;
+
+function gameTimer() {
+  if (!isGameOver) {
+    setTimeout(() => {
+      time++;
+      timer.innerHTML = time;
+      gameTimer();
+    }, 1000);
+  }
+}
+
+function stopTimer() {
+  if (isGameOver) {
+    clearTimeout(gameTimer);
+    time = 0;
+  }
+}
 
 //lets create gaming board
 const createBoard = () => {
   const bombSquares = Array(bombs).fill("boom");
   const emptySquares = Array(width * width - bombs).fill("number");
   const gameSquares = emptySquares.concat(bombSquares);
-  const randomSquares = gameSquares.sort(() => {
-    return Math.random() - 0.4;
+
+  const testingArray = gameSquares.map((square) => {
+    let random = Math.floor(Math.random() * gameSquares.length);
+    return { random: random, square: square };
   });
 
-  /*   SHUFFLE ARRAY AND RETURN IT BACK */
-
-  /*   var original = ["a", "b", "c", "d", "e", "f", "g"];
-  var copy = [].concat(original);
-  copy.sort(function () {
-    return 0.5 - Math.random();
+  const randomSquares = testingArray.sort((square1, square2) => {
+    return square2.random - square1.random;
   });
-  console.log(copy, "mapped"); */
-
-  console.log(randomSquares);
 
   for (let i = 0; i < width * width; i++) {
     const square = document.createElement("div");
     square.setAttribute("id", i);
     /*in a line below we add class from the array above with its "word" and add it to the square. This is how we will change the look of the squares in the game. */
-    square.classList.add(randomSquares[i]);
+    /* square.classList.add(randomSquares[i]); */
+    square.classList.add(testingArray[i].square);
 
     grid.appendChild(square);
     squares.push(square);
@@ -39,8 +57,34 @@ const createBoard = () => {
     square.addEventListener("click", (e) => {
       showNumbers(square);
       gameOver(square);
+      didIwin(square);
+      gameTimer();
     });
+
+    square.oncontextmenu = (e) => {
+      e.preventDefault();
+      setFlag(square);
+      showCounter();
+    };
   }
+
+  // gamers board info
+  const showCounter = () => {
+    let flagCounter = bombs - flags;
+    flagsLeft.innerHTML = flagCounter;
+  };
+
+  const setFlag = (square) => {
+    if (!square.classList.contains("flag")) {
+      square.classList.add("flag");
+      flags++;
+      console.log(flags);
+    } else {
+      square.classList.remove("flag");
+      flags--;
+      console.log(flags);
+    }
+  };
 
   // adding numbers to the square
   for (let i = 0; i < squares.length; i++) {
@@ -99,13 +143,17 @@ const showNumbers = (square) => {
   if (square.classList.contains("numbers") || square.classList.contains("zero"))
     return;
 
-  if (square.classList.contains("boom")) {
-    /*  gameText.style.border = "dotted 4px red";
-    gameText.innerHTML = "GAME OVER"; */
-    return;
-  } else {
+  if (square.classList.contains("boom")) return;
+  else {
     if (numbers != 0) {
       square.classList.add("numbers");
+
+      if (numbers == 1) square.classList.add("one");
+      if (numbers == 2) square.classList.add("two");
+      if (numbers == 3) square.classList.add("three");
+      if (numbers == 4) square.classList.add("four");
+      if (numbers == 5) square.classList.add("five");
+
       square.innerText = numbers;
       return;
     } else {
@@ -171,31 +219,53 @@ function checkSquare(square, id) {
 const gameOver = (square) => {
   if (square.classList.contains("boom")) {
     square.classList.add("show-boom");
-    gameText.style.border = "dotted 4px red";
     gameText.innerHTML = "GAME OVER";
     grid.style.pointerEvents = "none";
-    gameEnd = true;
+    isGameOver = true;
+    stopTimer();
   }
+
+  squares.forEach((square) => {
+    if (isGameOver && square.classList.contains("boom")) {
+      setTimeout(() => {
+        square.classList.add("show-boom");
+        square.classList.remove("flag");
+      }, 300);
+    }
+  });
 };
 
 // restart game
-const restartGame = (square) => {
-  if (gameEnd) {
-    square.classList.remove("boom");
-    square.classList.remove("number");
+const restartGame = () => {
+  squares.forEach((square) => {
+    square.classList.remove("show-boom");
+    square.classList.remove("numbers");
     square.classList.remove("zero");
+    square.classList.remove("flag");
+    square.innerHTML = "";
     gameText.innerHTML = "";
-    grid.style.pointerEvents = "all";
-    gameEnd = false;
+    grid.style.pointerEvents = "auto";
+    isGameOver = false;
+    flags = 0;
+    flagsLeft.innerHTML = bombs;
+    stopTimer();
+  });
+};
+
+restartBtn.addEventListener("click", restartGame);
+
+const didIwin = (square) => {
+  if (
+    flags === bombs &&
+    !isGameOver &&
+    square.classList.contains("boom") &&
+    square.classList.contains("flag")
+  ) {
+    gameText.innerHTML = "YOU WON THE GAME";
   }
 };
 
-restartBtn.addEventListener("click", () => {
-  location.reload();
-  return false;
-});
-
-let rightClick = new MouseEvent("contextmenu", {
+/* let rightClick = new MouseEvent("contextmenu", {
   bubbles: true,
   cancelable: true,
   view: window,
@@ -204,8 +274,10 @@ let rightClick = new MouseEvent("contextmenu", {
   clientX: grid.getBoundingClientRect().x,
   clientY: grid.getBoundingClientRect().y,
 });
+ */
 
-grid.oncontextmenu = (e) => {
+/* grid.oncontextmenu = (e, square) => {
   e.preventDefault();
-  grid.dispatchEvent(rightClick);
-};
+  square.classList.add("flag");
+  square.dispatchEvent(rightClick);
+}; */
